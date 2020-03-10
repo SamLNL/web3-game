c9e3cf7f7cc33985df7d9b03d3dc00ea717d3714
Sam <sam@emotion.nl>
2020-03-10 11:33:24 +0100

    New platforma blocks

--- src/images/favicon/readme.md
+++
@@ -1,9 +0,0 @@
-# Favicon
-
-Place a decently sized (512x512) favicon image here. The favicons task
-will generate a set of favicons from this source image.
-
-## See also
-
-- `tools/gulp/tasks/favicons.js` for the gulp task
-- `paths.favicon` in the `package.json` for the globs
--- src/js/bootstrap/blocks/menu.js
+++ src/js/bootstrap/blocks/menu.js
@@ -1,12 +1,13 @@
 (function() {
-
-    const toggle = document.querySelector('[data-id="menu-sidebar-toggle"]');
-    const close = document.querySelector('[data-id="menu-sidebar-close"]');
+console.log('hallo');
+    const toggle = document.querySelector('.navbar-toggler');
+    const close = document.querySelector('.navbar-side');
 
     if(toggle) {
         toggle.addEventListener('click', function(e){
+          console.log('1');
             e.preventDefault();
-            let sidebar = document.querySelector('#menu-sidebar');
+            let sidebar = document.querySelector('.navbar-collapse');
             if(sidebar){
                 sidebar.classList.toggle('show');
             }
@@ -16,11 +17,10 @@
     if(close) {
         close.addEventListener('click', function(e){
             e.preventDefault();
-            let sidebar = document.querySelector('#menu-sidebar');
+            let sidebar = document.querySelector('.navbar-collapse');
             if(sidebar){
                 sidebar.classList.remove('show');
             }
         });
     }
-
 })();
