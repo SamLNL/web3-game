926110be252db47ff6b36a540a8a0c07da01674c
Sam <sam@emotion.nl>
2020-03-19 14:01:29 +0100

    contact block edit

--- src/js/bootstrap/blocks/faq.js
+++ src/js/bootstrap/blocks/faq.js
@@ -3,7 +3,6 @@ const faqs = document.querySelectorAll('.js-faq');
 
 for(let i = 0;i < faqs.length;i++) {
   faqs[i].addEventListener('click', function(e){
-    e.preventDefault();
     sendGAEvent(['FAQ','click', e.currentTarget.getAttribute('title')]);
   });
 }
