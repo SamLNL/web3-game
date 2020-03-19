97040e92386ca6a7e10e8a111355966d143535f8
Sam <sam@emotion.nl>
2020-03-19 14:45:30 +0100

    content added + accessibility fixes

--- src/js/bootstrap/blocks/faq.js
+++ src/js/bootstrap/blocks/faq.js
@@ -5,4 +5,11 @@ for(let i = 0;i < faqs.length;i++) {
   faqs[i].addEventListener('click', function(e){
     sendGAEvent(['FAQ','click', e.currentTarget.getAttribute('title')]);
   });
+
+  faqs[i].addEventListener('keyup', function(e){
+    // Number 13 is the "Enter" key on the keyboard
+    if (e.keyCode === 13) {
+      faqs[i].firstChild.click();
+    }
+  });
 }
