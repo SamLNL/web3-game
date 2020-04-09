29c08d94b82fce219625ef6b592c3d06a1998f7b
Sam <sam@emotion.nl>
2020-04-09 11:02:13 +0200

    event tracking faq fixed

--- src/js/bootstrap/blocks/faq.js
+++ src/js/bootstrap/blocks/faq.js
@@ -2,7 +2,16 @@ import {sendGAEvent} from '../../analytics/analytics';
 const faqs = document.querySelectorAll('.js-faq');
 
 for(let i = 0;i < faqs.length;i++) {
-  faqs[i].addEventListener('click', function(e){
-    sendGAEvent(['FAQ','click', e.currentTarget.getAttribute('title')]);
+  faqs[i].addEventListener('click', function(){
+
+    // Makes sure only fires when faq is opening
+    let el = this;
+    setTimeout(function(){
+      let selector = el.querySelector('div.collapse');
+
+      if(selector && selector.classList.contains('show')){
+        sendGAEvent(['FAQ','click', el.getAttribute('data-ga-question')]);
+      }
+    }, 500);
   });
 }
