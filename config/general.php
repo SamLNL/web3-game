02fb4dacda3fa11dfc00e80d1ea1b3581e05f67e
Thomas <thomas@emotion.nl>
2020-09-08 18:00:43 +0200

    Default dev mode on local

--- config/general.php
+++ config/general.php
@@ -112,7 +112,7 @@ return [
     'allowUpdates' => true,
     'backupOnUpdate' => true,
     'allowAdminChanges' => true,
-    'devMode' => false,
+    'devMode' => true,
     'enableTemplateCaching' => true,
     'isSystemLive' => true,
     // Aliases parsed in sites’ settings, volumes’ settings, and Local volumes’ settings
